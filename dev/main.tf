e01e25a0f222012e58592a64a159a0c8037b1dc1
TechSwitch <160839453+techswitch-c@users.noreply.github.com>
2024-06-07 12:22:42 +0530

    module for IAM role

---
+++ dev/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+###############################
+# IAM assumable role for admin
+###############################
+module "iam_assumable_role_admin" {
+  source = "../"
+
+  allow_self_assume_role = true
+
+  trusted_role_arns = [
+    "arn:aws:iam::307990089504:root",
+    "arn:aws:iam::835367859851:user/anton",
+  ]
+
+  trusted_role_services = [
+    "codedeploy.amazonaws.com"
+  ]
+
+  create_role             = true
+
+  role_name         = "admin"
+
+  attach_admin_policy = true
+
+  tags = {
+    Role = "Admin"
+  }
+}
